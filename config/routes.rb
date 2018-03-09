40d26aeed095124e615173f35bf749f9fdc56ca9
Blake Arnsdorff <blake@dontpanicux.com>
2018-03-09 09:24:01 -0800

    removed nav from individual pages. Added nav to applicaiton.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   root 'pages#home'
   get 'about' => 'pages#about'
-  get 'contact' => 'pages#contact'
+  get 'contactus' => 'pages#contactus'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
